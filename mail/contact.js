d789ff284a709ff4c6cfc0a99ce9fd0fdd5bce9a
tembaestetica <tembaestetica@gmail.com>
2025-03-22 12:14:02 +0100

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -1,8 +1,8 @@
 $(function () {
-
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
+            // Manejo de errores si es necesario
         },
         submitSuccess: function ($form, event) {
             event.preventDefault();
@@ -29,16 +29,21 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Tu mensaje ha sido enviado.</strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
+
+                    // Enviar un correo a Gmail con los mismos detalles
+                    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+                    window.open(mailtoLink, '_blank');
+                    
                 },
                 error: function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Perdona " + name + ", parece que nuestro servidor de correo no está respondiendo. Por favor, inténtelo de nuevo más tarde!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
